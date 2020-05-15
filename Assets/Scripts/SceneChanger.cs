f7962d461b6c31438fa9fe973442e49cb632a880
Ean Daus <Eandaus123@gmail.com>
2020-05-15 15:12:33 -0700

    Integrated Map view and implemented navigation between map and AR views

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+
+    public void ARScene()
+    {
+        SceneManager.LoadScene("ARView");
+    }
+
+    public void HomeScene()
+    {
+        SceneManager.LoadScene("HomeView");
+    }
+
+    public void MapScene()
+    {
+        SceneManager.LoadScene("MapView");
+    }
+}
